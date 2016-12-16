456501c22b2d9a26290f55c58b765c69f197f8cb
LIN-CHIING <missching1996@gmail.com>
2016-12-16 23:57:38 +0800

    Add files via upload

---
+++ _1214-4/_1214-4.ino
@@ -0,0 +1,96 @@
+//MCU
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>
+
+const char* ssid = "IOTCourse";
+const char* password = "";
+
+ESP8266WebServer server(80);
+
+const int led = 13;
+char led_status[25];
+String webSite;
+byte x = 0;
+void buildWeb() {
+  webSite += "<html>";
+  webSite+="<head>\n";
+  webSite+="<meta charset='UTF-8'/>\n";
+  webSite+="<title>林靜映如的Motor實驗</title>\n";
+  webSite+="<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>";
+  webSite+="<script>";
+  webSite+="$(document).ready(function(){";
+  webSite+="$(\"[type = range]\").change(function(){";
+  webSite+="var redval = $(\"#red\").val();";
+  webSite+="$(\"#red-text\").text(redval);";
+  webSite+="});";
+  webSite+="});";
+  webSite+="</script>";
+    webSite += "<style>";
+  webSite += "body{background-color:#D1BBFF; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;}";
+  webSite += "div{width:500px; text-align: center; margin: 50 auto;}";
+  webSite += "</style>";
+  webSite+="</head>\n";
+  webSite += "<body>";
+  webSite += "<h1>林靜映如的Motor實驗</h1>";
+  webSite += "<h2>173520 <3</h2>";
+  webSite += "<form action=\"/form1\">";
+  webSite += "<p>angle : <span id = \"red-text\">0</span></p>";
+  webSite += "<input type=\"range\" id=\"red\" name=\"R-led\" min=\"0\" max=\"179\" value=\"0\">";
+  webSite += "<input type=\"submit\" value=\"submit\"></form>";
+  webSite += "</body></html>";
+   
+}
+
+void handleRoot() {
+  server.send ( 200, "text/html", webSite ); //send back client a HTML file
+}
+
+void handleLEDStatus() {
+  int red_color = server.arg("R-led").toInt();
+  Wire.beginTransmission(8); // transmit to device #8
+      // sends five bytes
+  Wire.write(red_color);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+}
+
+
+void handleNotFound() {
+  server.send ( 404, "text/html", "404 Error!" );  
+}
+void setup(void){
+  buildWeb();
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on ( "/", handleRoot );
+  server.on("/form1",handleLEDStatus);
+  server.onNotFound ( handleNotFound );
+  Wire.begin(4,5);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
+
+
---
+++ _1214-slave/_1214-slave.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+// 使用可變電阻控制 Servo 的旋轉角度
+// by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
+
+// 引用 Servo Library
+#include <Servo.h> 
+
+// 建立一個 Servo 物件 
+Servo myservo; 
+ 
+void setup() {
+  myservo.attach(9);
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(115200);           // start serial for output
+}
+
+void loop() {
+  delay(1500);
+
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+    int c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  myservo.write(c);                  // 設定 Servo 旋轉角度
+
+  delay(15);                           // 等待 Servo 旋轉指定的位置
+
+
+}
+
